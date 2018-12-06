d2f9850a043a5f770b92f34597c8ccadc2a2b1eb
RomanLevkovych <rlevkovych098@gmail.com>
2018-12-06 14:47:18 +0200

    Initial commit for SongKick

---
+++ SongKick/SongKick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SongKick
+//
+//  Created by Roman on 12/6/18.
+//  Copyright © 2018 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
